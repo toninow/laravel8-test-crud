11c00e03571fd8675e8461f729f01c89631adf43
toninow <abenalcazarc@gmail.com>
2022-05-17 18:54:34 -0500

    cart payments and order model factory and seeders added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-4 mx-auto">
             <div class="card card-body mb-2">
-                <h1 align="center">Bienvenido a Laravel</h1>
+                <h1 align="center">Bienvenido a su Ecommerce</h1>
             </div>
         </div>
     </div>
